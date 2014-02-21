935d0d4422af406b19d8ca49a2001205db4b2a52
Yumeng Sun <yxs9493@rit.edu>
2014-02-21 16:52:52 -0500

    Classes and empty functions
    need implementations ASAP

---
+++ js/class/entry.js
@@ -0,0 +1,13 @@
+/** 
+ * entry class
+ * 
+ * 
+ */
+function Entry(name){
+    this.name = name;
+    this.checked = false;
+    this.description = null;
+    this.reminder = null;
+    this.deleted = false;
+    
+}
---
+++ js/main.js
@@ -0,0 +1,31 @@
+/**
+ *  Main functionality. UI interaction should be in ../script.js. 
+ */
+
+var entries = new Array();
+
+
+
+/** Retrieve data from local storage
+ */
+function retrieveData(){
+    
+}
+
+/** populate entries into differnet catagories.
+ */
+function populateCatagories(){
+    
+}
+
+/** "Delete"(hide) entry
+ */
+function deleteEntry(){
+    
+}
+
+/** Check/uncheck entry
+ */
+function checkEntry(){
+    
+}
