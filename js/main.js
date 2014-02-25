86cccc77dbd1c7c74db963ca27e9aaa0fcc20aa0
Yumeng Sun <yxs9493@rit.edu>
2014-02-25 07:47:55 -0500

    behavior
    some behavior added

--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@
  */
 
 var entries = new Array();
-
+var today = new Array();
+var tomorrow = new Array();
+var today = new Array();
+var yesterday = new Array();
+var completed = new Array();
+localInitialization();
 
 
 /** Retrieve data from local storage
  */
 function retrieveData(){
-    
+    window.localStorage.get
 }
 
 /** populate entries into differnet catagories.
@@ -20,12 +25,29 @@ function populateCatagories(){
 
 /** "Delete"(hide) entry
  */
-function deleteEntry(){
-    
+function deleteEntry(id){
+    window.localStorage.removeItem(id);
 }
 
 /** Check/uncheck entry
  */
-function checkEntry(){
-    
+function checkEntry(id){
+    var thisItem = window.localStorage.getItem(id);
+    thisItem.check = true;
+    window.localStorage.setItem(id, thisItem);
+}
+
+/** Save entry
+ */
+function saveEntry(id, obj){
+	window.localStorage.setItem(id, obj);
+}
+
+function localInitialization(){
+	if(window.localStorage){
+		console.log("browser support localStorage");
+	}else{
+		console.log('cookie please~ :3');
+	}
+
 }
